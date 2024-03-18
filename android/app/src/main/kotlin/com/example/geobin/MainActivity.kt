b3e479e13ee58620c116bbb9899d8a0d4e6777ce
Madhav Gupta <114633095+M4dhav@users.noreply.github.com>
2024-03-18 14:36:12 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/geobin/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.geobin
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/landingpage.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:geobin/homepage.dart';
+
+class LandingPage extends StatefulWidget {
+  const LandingPage({super.key});
+
+  @override
+  State<LandingPage> createState() => _LandingPageState();
+}
+
+class _LandingPageState extends State<LandingPage> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Scaffold(
+      appBar: AppBar(
+        title: Center(child: Text("Login Page")),
+        backgroundColor: Colors.green,
+      ),
+      body: Center(
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+            Column(
+              children: [
+                Text(
+                  "Welcome to                      ",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+                ),
+                Text(
+                  "           Geobin!",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+                ),
+              ],
+            ),
+            Column(
+              children: [
+                ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) => HomePage()));
+                    },
+                    child: Text("Login")),
+                ElevatedButton(onPressed: () {}, child: Text("Sign Up")),
+              ],
+            )
+          ])),
+    ));
+  }
+}
+//206 bla 1:30 - 3:30
